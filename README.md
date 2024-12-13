cb9ac4c44b7c157602833222c7200d120ddb1a35
Aaron <yangtao.yangtao@antgroup.com>
2024-12-13 15:08:15 +0800

    chore: add script to generate readme

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# awards
 
-Grant the AntV badge to GitHub users
+# AntV Awards
+
+This repository records the badges granted to the Github users who have made outstanding contributions to the AntV project.
+
+
+## Usage
+
+1. Query your award information under this document based on the GitHub ID.
+
+2. Generate the badge URL:
+
+> <span>\!\[\]\(https://img.shields.io/endpoint?url=https://antv.vision/awards/&lt;github_id&gt;-&lt;badge&gt;.json\)</span>
+
+Example:
+
+![](https://img.shields.io/endpoint?url=https://antv.vision/awards/aarebecca-g6-maintainer.json)
+
+
+## Badges
+
+| Badge | Description | Achievement |
+| - | - | - |
+|`g-maintainer`|AntV G 维护者|_参与 AntV G 项目维护_|
+|`g-contributor`|AntV G 贡献者|_为 AntV G 项目提交 PR 并被合并_|
+|`g2-maintainer`|AntV G2 维护者|_参与 AntV G2 项目维护_|
+|`g2-contributor`|AntV G2 贡献者|_为 AntV G2 项目提交 PR 并被合并_|
+|`g6-maintainer`|AntV G6 维护者|_参与 AntV G6 项目维护_|
+|`g6-contributor`|AntV G6 贡献者|_为 AntV G6 项目提交 PR 并被合并_|
+|`g6-skilled`|熟练使用 AntV G6|_参与 2024 年 AntV G6 5.0 知识问答活动并获得 90 分以上_|
+|`l7-maintainer`|AntV L7 维护者|_参与 AntV L7 项目维护_|
+|`l7-contributor`|AntV L7 贡献者|_为 AntV L7 项目提交 PR 并被合并_|
+
+## Awards
+
+| GitHub ID | Award | Date |
+| - | - | - |
+|hustcc|G2 Contributor||
+|hustcc|G6 Contributor|2024-01-01|
+|aarebecca|G6 Maintainer||
+|yvonneyx|G6 Maintainer||
---
+++ readme.js
@@ -0,0 +1,62 @@
+const { writeFileSync } = require('fs');
+const {
+  loadJsonFile,
+  parseList,
+  parseConfig,
+  normalizeBadge,
+} = require('./utils');
+
+const config = parseConfig(loadJsonFile('config.json'));
+const list = parseList(loadJsonFile('list.json'));
+
+const description = `
+# AntV Awards
+
+This repository records the badges granted to the Github users who have made outstanding contributions to the AntV project.
+`;
+
+const usages = `
+## Usage
+
+1. Query your award information under this document based on the GitHub ID.
+
+2. Generate the badge URL:
+
+> <span>\\!\\\[\\]\\(https://img.shields.io/endpoint?url=https://antv.vision/awards/&lt;github_id&gt;-&lt;badge&gt;.json\\)</span>
+
+Example:
+
+![](https://img.shields.io/endpoint?url=https://antv.vision/awards/aarebecca-g6-maintainer.json)
+`;
+
+const awards = [];
+Object.entries(list).forEach(([id, badges]) => {
+  badges.forEach(({ badge, date }) => {
+    awards.push([id, badge, date]);
+  });
+});
+
+const badges = Object.entries(config.badges)
+  .map(
+    ([badge, { description, achievement }]) =>
+      `|\`${normalizeBadge(badge)}\`|${description}|_${achievement}_|`
+  )
+  .join('\n');
+
+const table = `
+## Badges
+
+| Badge | Description | Achievement |
+| - | - | - |
+${badges}
+
+## Awards
+
+| GitHub ID | Award | Date |
+| - | - | - |
+${awards
+  .map(([id, badge, date]) => `|${id.toLowerCase()}|${badge}|${date}|`)
+  .join('\n')}
+`;
+
+writeFileSync('README.md', [description, usages, table].join('\n'));
